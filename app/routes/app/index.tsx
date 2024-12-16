3455903c89f077e75e624a9aa950d09e6821ab04
Nikita <github@nikiv.dev>
2024-12-16 02:54:25 +0100

    pls build

--- app/routes/app/index.tsx
+++ app/routes/app/index.tsx
@@ -2,9 +2,9 @@ import { createFileRoute } from "@tanstack/react-router"
 import { useLaunchParams } from "@telegram-apps/sdk-react"
 
 function RouteComponent() {
-  // const lp = useLaunchParams()
-  // const startParam = lp.initData!.startParam
-  // console.log(startParam, "start param")
+  const lp = useLaunchParams()
+  const startParam = lp.initData!.startParam
+  console.log(startParam, "start param")
   return (
     <>
       <div className="text-white-500">mini app</div>
