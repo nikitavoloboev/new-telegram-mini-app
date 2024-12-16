ecee3ef3f888b53313b5d0d3a74fe8cffd98af61
Nikita <github@nikiv.dev>
2024-12-16 01:03:21 +0100

    try start param

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { useLaunchParams } from "@telegram-apps/sdk-react"
 
 function RouteComponent() {
+  const lp = useLaunchParams()
+  const startParam = lp.initData!.startParam
+  console.log(startParam, "start param")
   return (
     <>
       <div className="text-red-500">test</div>
