b6524133d7520b5646be419d90249311d5e8f631
Nikita <github@nikiv.dev>
2025-02-13 17:03:50 +0100

    new v3 way

--- app/routes/app/index.tsx
+++ app/routes/app/index.tsx
@@ -1,21 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router"
-import { useLaunchParams } from "@telegram-apps/sdk-react"
+import { retrieveLaunchParams } from "@telegram-apps/sdk-react"
 import { useMemo } from "react"
 
 // TODO: need way to get mocked initData
 // and in prod work as usual too
 function RouteComponent() {
-  const lp = useLaunchParams()
-  // TODO: maybe no need for useMemo?
-  const telegramId = useMemo(
-    () => lp.initData!.user!.id.toString(),
-    [lp.initData],
-  )
-  const telegramUsername = useMemo(
-    () => lp.initData!.user!.username,
-    [lp.initData],
-  )
-  console.log(telegramId, telegramUsername)
+  const lp = useMemo(() => retrieveLaunchParams(true), [])
+  console.log(lp)
 
   return (
     <>
